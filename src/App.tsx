8cd24183965dfd4e37ffa6f6c2863f2da190eb46
John Doe <mitchellcaleb00@gmail.com>
2022-01-20 06:48:01 -0800

    feat: Return as initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import HeaderPanel from './components/ui/headerPanel';
 function App() {
   return (
-    <div className="wrap">
-      <HeaderPanel></HeaderPanel>
-    </div>
-  );
+        <>
+          App
+        </>
+    );
 }
 
 export default App;
